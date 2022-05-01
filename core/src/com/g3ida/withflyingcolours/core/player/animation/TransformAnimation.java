bb90329b450691e811cf79544d3449c652f5f631
g3ida <med.gaida@gmail.com>
2022-05-01 12:36:12 +0100

    fix transform animation with various angles

--- core/src/com/g3ida/withflyingcolours/core/player/animation/TransformAnimation.java
+++ core/src/com/g3ida/withflyingcolours/core/player/animation/TransformAnimation.java
@@ -1,6 +1,7 @@
 package com.g3ida.withflyingcolours.core.player.animation;
 
 import com.badlogic.gdx.math.Interpolation;
+import com.g3ida.withflyingcolours.Utils;
 
 import games.rednblack.editor.renderer.components.TransformComponent;
 
@@ -34,15 +35,23 @@ public class TransformAnimation {
 
     public void step(TransformComponent transform, float deltaTime) {
         if (_timer > 0f) {
-            float normalized = _timer / PlayerAnimationComponent.SQUEEZE_ANIM_DURATION;
+
+            // if the player is rotated we should react according to the actual direction
+            float cosRotation = (float)Math.cos(transform.rotation * Utils.PI / 180.);
+            float sinRotation = (float)Math.sin(transform.rotation * Utils.PI / 180.);
+
+            float normalized = _timer / _animationDuration;
             float i = _interpolation.apply(0f, 1f, normalized);
-            transform.scaleX = i;
-            transform.scaleY = 1f / (i + 0.01f);
-            if (!_centerOriginY) {
-                transform.originY = 0.5f + (1f - transform.scaleY) * 0.5f;
+            transform.scaleX = i * Math.abs(cosRotation) +  Math.abs(sinRotation) / (i + 0.01f);
+            transform.scaleY = i * Math.abs(sinRotation) +  Math.abs(cosRotation) / (i + 0.01f);
+
+            if (!this._centerOriginY) {
+                transform.originY = 0.5f + (1f - transform.scaleY) * 0.5f * cosRotation;
+                transform.originX = 0.5f + (1f - transform.scaleX) * 0.5f * sinRotation;
             }
-            if (!_centerOriginX) {
-                transform.originX = 0.5f + (1f - transform.scaleX) * 0.5f;
+            if (!this._centerOriginX) {
+                transform.originX = 0.5f + (1f - transform.scaleX) * 0.5f * cosRotation;
+                transform.originY = 0.5f + (1f - transform.scaleY) * 0.5f * sinRotation;
             }
             _timer -= deltaTime;
         } else {
