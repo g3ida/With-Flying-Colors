11e848152efa97a5cc60cfb69f8c66049ad60fc0
g3ida <med.gaida@gmail.com>
2022-05-29 09:05:42 +0100

    fix wall sliding

--- core/src/com/g3ida/withflyingcolours/Constants.java
+++ core/src/com/g3ida/withflyingcolours/Constants.java
@@ -3,4 +3,9 @@ package com.g3ida.withflyingcolours;
 public class Constants {
     public static final String LOG_SHADER_PREFIX = "GLSL";
     public static final String SHADERS_PATH = "shaders/";
+
+    public static class Game {
+        public static final float PLAYER_WALL_FRICTION = 0.06f;
+        public static final float PLAYER_GROUND_FRICTION = 1f;
+    }
 }
--- core/src/com/g3ida/withflyingcolours/core/scripts/ColorPlatform.java
+++ core/src/com/g3ida/withflyingcolours/core/scripts/ColorPlatform.java
@@ -3,9 +3,11 @@ package com.g3ida.withflyingcolours.core.scripts;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
+import com.g3ida.withflyingcolours.Constants;
+import com.g3ida.withflyingcolours.Utils;
 import com.g3ida.withflyingcolours.core.platform.ColorPlatformRenderingComponent;
-import com.g3ida.withflyingcolours.core.platform.ColorPlatformRenderingSystem;
 
+import games.rednblack.editor.renderer.components.physics.PhysicsBodyComponent;
 import games.rednblack.editor.renderer.physics.PhysicsContact;
 import games.rednblack.editor.renderer.utils.ComponentRetriever;
 
@@ -46,6 +48,14 @@ public class ColorPlatform extends GameScript implements PhysicsContact {
     public void beginContact(int contactEntity, Fixture contactFixture, Fixture ownFixture, Contact contact) {
         _platformRenderingComponent.doColorSplash = true;
         _platformRenderingComponent.contactPosition = contactFixture.getBody().getPosition();
+
+        // solve the bug of the player sticking to the walls instead of falling
+        PhysicsBodyComponent physicsBody = ComponentRetriever.get(contactEntity, PhysicsBodyComponent.class, getEngine());
+        if (physicsBody != null && Math.abs(physicsBody.body.getLinearVelocity().y) > Utils.epsilon) {
+            contact.setFriction(Constants.Game.PLAYER_WALL_FRICTION);
+        } else {
+            contact.setFriction(Constants.Game.PLAYER_GROUND_FRICTION);
+        }
     }
 
     @Override
